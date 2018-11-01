11d8b114e0efec3a87bd8325b87bb3a115c73567
Nguyễn Ngọc Ánh <anhnnd00639@fpt.edu.vn>
2018-11-01 14:50:15 +0700

    ADDDELETE

---
+++ AddEditDelete/AddEditDelete/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using AddEditDelete.Models;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+using System.Linq;
+using AddEditDelete.Models;
+
+namespace APP.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Update(Product product)
+        {
+            var existProduct = _context.Products.Find(product.id);
+            if(existProduct == null)
+            {
+                return NotFound();
+            }
+            existProduct.name = product.name;
+            existProduct.price = product.price;
+            _context.Products.Update(existProduct);
+            _context.SaveChanges();
+            TempData["Message"] = "Update Success";
+            return Redirect("Create");
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Create()
+        {
+            return View(_context.Products.ToList());
+
+        }
+
+        [HttpPost]
+        public IActionResult Save(Product pr)
+        {
+            _context.Products.Add(pr);
+            _context.SaveChanges();
+            return new RedirectResult("Create");
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            var exisProduct = _context.Products.Find(product.id);
+            if (exisProduct == null)
+            {
+                return NotFound();
+            }
+
+            exisProduct.name = product.name;
+            exisProduct.price = product.price;
+            _context.Products.Update(exisProduct);
+            _context.SaveChanges();
+
+            return new RedirectResult("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, Product product)
+        {
+            var exisProduct = _context.Products.Find(product.id);
+            if (exisProduct == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(exisProduct);
+            _context.SaveChanges();
+
+            return new RedirectResult("Create");
+        }
+        public IActionResult Delete(int ID)
+        {
+            return new JsonResult(new Product
+            {
+                id = ID
+            });
+        }
+    }
+}
---
+++ AddEditDelete/AddEditDelete/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNetCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddEditDelete.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
