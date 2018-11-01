4040f27e564090931b0c59f9a5eede8d6df4a21b
Nguyễn Ngọc Ánh <anhnnd00639@fpt.edu.vn>
2018-11-01 14:36:26 +0700

    Lab1

---
+++ MyNetCoreMVC/MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetCoreMVC.Models;
+
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+
+    {
+        private readonly Product _context;
+
+        public ProductController(Product context)
+        {
+            _context = context;
+        }
+        public IActionResult GetList()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            ViewData["Name"] = "Đây là trường Name";
+            ViewData["Price"] = "Đây là trường Price";
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+
+        }
+        public IActionResult Delete(int ID)
+        {
+
+            return View();
+        }
+    }
+}
+
